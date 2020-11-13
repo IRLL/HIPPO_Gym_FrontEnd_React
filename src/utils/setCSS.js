7c43a464f6ff0da5227628a5aae085fd2f7470e6
Yuan <wang17@ualberta.ca>
2020-11-13 20:39:39 +0800

    Remove setCSS funciton

--- src/utils/setCSS.js
+++
@@ -1,9 +0,0 @@
-export default function setCSS(cssPath) {
-    let head  = document.getElementsByTagName('head')[0];
-    let link  = document.createElement('link');
-    link.rel  = 'stylesheet';
-    link.type = 'text/css';
-    link.href = cssPath;
-    link.media = 'all';
-    head.appendChild(link);
-}
