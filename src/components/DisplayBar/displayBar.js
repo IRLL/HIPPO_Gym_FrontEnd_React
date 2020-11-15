11c9d8f75e3777f94a5ec37392f8bbfa8c3496ef
Yuan <wang17@ualberta.ca>
2020-11-15 21:01:14 +0800

    Add display bar to show arbitray information

---
+++ src/components/DisplayBar/displayBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import './displayBar.css';
+import capitalize from '../../utils/capitalize';
+
+class DisplayBar extends React.Component{
+
+    render(){
+        const {visible, isLoading, displayData} = this.props;
+
+        let messageList = [];
+        for(let key in displayData){
+            messageList.push(
+                <div className="displayMessage" ><strong>{capitalize(key)}</strong> : {displayData[key]}</div>
+            )
+        }
+
+        return (
+            <div >
+                {visible && !isLoading ? 
+                <div className="displayContainer">
+                    {messageList}
+                </div> : null}
+            </div>
+        )
+    }
+}
+
+export default DisplayBar;
