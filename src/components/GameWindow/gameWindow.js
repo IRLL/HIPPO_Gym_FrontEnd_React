3c6f43eb4b708a98278b297c3a74966045c3dd1c
Yuan Wang <wang17@ualberta.ca>
2021-02-28 22:10:11 +0800

    Vertically center aligns the image components

--- src/components/GameWindow/gameWindow.js
+++ src/components/GameWindow/gameWindow.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Progress, Row, Col} from 'antd';
+import {Progress} from 'antd';
 import './gameWindow.css';
 import 'antd/dist/antd.css';
 
@@ -10,29 +10,23 @@ class GameWindow extends React.Component {
         const {isLoading, frameSrc, progress, imageL, imageR} = this.props;
 
         return (
-            <Row>
-                <Col flex={1} >
-                    <div className="imageContainer" > 
-                        {imageL ? <img src={imageL} className="imageComponent" alt="imageLeft" width="400px" height="400px" /> : null}
-                    </div>
-                </Col>
-                <Col flex={2} >
-                    <div className= "gameWindow" >
-                        {isLoading || !frameSrc ?
-                            <div className="progressBar">
-                                <Progress width={80} type="circle" percent={Math.round(progress)}/>
-                                <p className="promptText">The robot is about to start the game, please wait ...</p> 
-                            </div>
-                            : <img className="gameContent" src={frameSrc} alt="frame" width="700px" height="600px" />
-                        }
-                    </div>
-                </Col>
-                <Col flex={1} > 
-                    <div className="imageContainer" > 
-                        { imageR ? <img src={imageR} className="imageComponent" alt="imageRight" width="400px" height="400px" /> : null}
-                    </div> 
-                </Col>
-            </Row>
+            <div className="container" >
+                <div className="imageContainer" > 
+                    {imageL ? <img src={imageL} className="imageComponent" alt="imageLeft" width="400px" height="400px" /> : null}
+                </div>
+                <div className= "gameWindow" >
+                    {isLoading || !frameSrc ?
+                        <div className="progressBar">
+                            <Progress width={80} type="circle" percent={Math.round(progress)}/>
+                            <p className="promptText">The robot is about to start the game, please wait ...</p> 
+                        </div>
+                        : <img className="gameContent" src={frameSrc} alt="frame" width="700px" height="600px" />
+                    }
+                </div>
+                <div className="imageContainer" > 
+                    {imageR ? <img src={imageR} className="imageComponent" alt="imageRight" width="400px" height="400px" /> : null}
+                </div> 
+            </div>
         )
     }
 }
