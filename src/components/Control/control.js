7975c170b95aa27e554111aa8630f8f9fd788639
PayasSingh <payas@ualberta.ca>
2021-06-22 16:46:45 -0600

    aspect ratio is maintained, control panel is fixed and responsive

--- src/components/Control/control.js
+++ src/components/Control/control.js
@@ -7,44 +7,18 @@ import capitalize from "../../utils/capitalize";
 import sentenceCase from "../../utils/sentenceCase";
 
 class ControlPanel extends React.Component {
-
-
 	render() {
-		const {
-			isEnd,
-			isLoading,
-			UIlist,
-			brightness,
-			contrast,
-			saturation,
-			hue,
-			instructions,
-			orientation,
-      DEBUG
-		} = this.props;
+		const {isEnd, isLoading, UIlist, brightness, contrast, saturation, hue,
+			instructions, orientation, DEBUG } = this.props;
 
-		const directions = [
-			"left",
-			"leftUp",
-			"up",
-			"rightUp",
-			"down",
-			"leftDown",
-			"rightDown",
-			"fire",
-			"right",
-		];
-		const commands = [
-			"start",
-			"pause",
-			"stop",
-			"reset",
-			"good",
-			"bad",
-			"trainOnline",
-			"trainOffline",
-		];
+		const directions = [ "left", "leftUp", "up", "rightUp", "down",
+			"leftDown", "rightDown", "fire", "right", ];
+
+		const commands = [ "start", "pause", "stop", "reset", "good",
+			"bad", "trainOnline", "trainOffline", ];
+
 		const fps = ["fpsUp", "fpsDown", "fpsSet"];
+
 		const imageControls = [
 			{ name: "brightness", min: 0, max: 1000, default: 100, ref: brightness },
 			{ name: "contrast", min: 0, max: 500, default: 100, ref: contrast },
@@ -53,16 +27,12 @@ class ControlPanel extends React.Component {
 		];
 
 		const instructionUI = [];
-		const imageCommands = [
-			"undo",
-			"redo",
-			"resetImage",
-			"submitImage",
-			"addMinutia",
-			"resetImage",
-			"submitImage",
-		];
+
+		const imageCommands = [ "undo",	"redo", "resetImage", "submitImage",
+      "addMinutia", "resetImage", "submitImage", ];
+
 		const defaultButtons = [...directions, ...fps];
+
 		const UIFiltered = UIlist.filter(
 			(ele) =>
 				!defaultButtons.includes(ele) &&
@@ -71,45 +41,45 @@ class ControlPanel extends React.Component {
 		);
 
 		const elements = {
-			fpsSet: (
-				<Col key="fpsSet" span={4}>
-					{UIlist.includes("fpsSet") ? (
-            <Tooltip placement="left" title="Press Enter to change fps" arrowPointAtCenter>
-              <Input
-                id="fpsSet"
-                className="fpsInput"
-                defaultValue={30}
-                value={this.props.inputFrameRate}
-                type="number"
-                suffix="FPS"
-                onChange={(e) => this.props.handleFPS("input",e.target.value)}
-                onPressEnter={(e) => this.props.handleFPS("enter", e.target.value)}
-              />
-            </Tooltip>
-					) : null}
-				</Col>
-			),
-			fpsUp: (
-				<Col key="fpsUp" span={4}>
-					{UIlist.includes("fpsUp") ? (
-						<Tooltip placement="top" title="Increase the FPS by 5" arrowPointAtCenter>
-							<Button
-								shape="round"
-								id="fpsUp"
-								className="fpsUpButton"
-								size="large"
-								icon={icons["fpsUp"]}
-								onClick={() => this.props.handleFPS("faster", null)}
-							>
-								Increase
-							</Button>
-						</Tooltip>
-					) : null}
-				</Col>
-			),
-			fpsDown: (
-				<Col key="fpsDown" span={4}>
-					{UIlist.includes("fpsDown") ? (
+      fpsSet:
+        UIlist.includes("fpsSet") ? (
+          <Col key="fpsSet">
+              <Tooltip placement="left" title="Press Enter to change fps" arrowPointAtCenter>
+                <Input
+                  id="fpsSet"
+                  className="fpsInput"
+                  defaultValue={30}
+                  value={this.props.inputFrameRate}
+                  type="number"
+                  suffix="FPS"
+                  onChange={(e) => this.props.handleFPS("input",e.target.value)}
+                  onPressEnter={(e) => this.props.handleFPS("enter", e.target.value)}
+                />
+              </Tooltip>
+          </Col>
+				) : null
+			,
+			fpsUp:
+				UIlist.includes("fpsUp") ? (
+          <Col key="fpsUp">
+              <Tooltip placement="top" title="Increase the FPS by 5" arrowPointAtCenter>
+                <Button
+                  shape="round"
+                  id="fpsUp"
+                  className="fpsUpButton"
+                  size="large"
+                  icon={icons["fpsUp"]}
+                  onClick={() => this.props.handleFPS("faster", null)}
+                >
+                  Increase
+                </Button>
+              </Tooltip>
+          </Col>
+				) : null
+			,
+			fpsDown:
+				UIlist.includes("fpsDown") ? (
+				  <Col key="fpsDown">
 						<Tooltip placement="bottom" title="Decrease the FPS by 5" arrowPointAtCenter>
 							<Button
 								shape="round"
@@ -122,50 +92,52 @@ class ControlPanel extends React.Component {
 								Decrease
 							</Button>
 						</Tooltip>
-					) : null}
-				</Col>
-			),
+            </Col>
+        ) : null
+			,
 		};
 		directions.forEach((dir) => {
-			elements[dir] = (
-				<Col key={dir} span={2}>
-					{UIlist.includes(dir) ? (
-						<Button
-							id={dir}
-							shape="round"
-							size="large"
-							icon={icons[dir]}
-							onClick={() => this.props.sendMessage({ actionType: "mousedown", action: dir })}
-						/>
-					) : null}
-				</Col>
-			);
-		});
-		commands.forEach((command) => {
-			elements[command] = (
-				<Col key={command}>
-					<Button
-						shape="round"
-						type="primary"
-						id={command}
-						className={`${command}Button`}
-						icon={icons[command]}
-						size="large"
-						onClick={() => this.props.handleCommand(command)}
-					>
-						{capitalize(command)}
-					</Button>
-				</Col>
-			);
+			elements[dir] =
+				UIlist.includes(dir) ? (
+          <Col key={dir} span={2}>
+              <Button
+                id={dir}
+                shape="round"
+                size="large"
+                icon={icons[dir]}
+                onClick={() => this.props.sendMessage({ actionType: "mousedown", action: dir })}
+                />
+          </Col>
+        ) : null
+        ;
+      });
+      commands.forEach((command) => {
+        elements[command] =
+        UIlist.includes(command) ? (
+          <Col key={command}>
+            <Button
+              shape="round"
+              type="primary"
+              id={command}
+              className={`${command}Button`}
+              icon={icons[command]}
+              size="large"
+              onClick={() => this.props.handleCommand(command)}
+              >
+              {capitalize(command)}
+            </Button>
+          </Col>
+        ) : null
+			;
 		});
 		instructions.forEach((instruction, i) => {
 			elements[`instruction${i}`] = <li key={`instruction${i}`}>{instruction}</li>;
 			instructionUI.push(elements[`instruction${i}`]);
 		});
 		imageControls.forEach((control) => {
-			elements[control.name] = (
-				<Col key={control.name} className="space-align-container" flex="1" align="center">
-					{UIlist.includes(control.name) && (
+			elements[control.name] =
+				UIlist.includes(control.name) && (
+				  <Col key={control.name} className="space-align-container" flex="1" align="center">
 						<div className="space-align-block imageControlTextContainer">
 							<Space align="center">
 								<span>{icons[control.name]}</span>
@@ -181,14 +153,13 @@ class ControlPanel extends React.Component {
 								onChange={(value) => this.props.handleImage(control.name, value)}
 							/>
 						</div>
-					)}
-				</Col>
-			);
+          </Col>
+        );
 		});
 		imageCommands.forEach((command) => {
-			elements[command] = (
-				<Col key={command}>
-					{UIlist.includes(command) && (
+			elements[command] =
+				UIlist.includes(command) && (
+				  <Col key={command}>
 						<Button
 							shape="round"
 							type="primary"
@@ -201,9 +172,8 @@ class ControlPanel extends React.Component {
 						>
 							{capitalize(sentenceCase(command))}
 						</Button>
-					)}
-				</Col>
-			);
+          </Col>
+        );
 		});
 		UIFiltered.forEach((ele) => {
 			if (!(ele in elements)) {
@@ -222,31 +192,42 @@ class ControlPanel extends React.Component {
 				);
 			}
 		});
+    const next =
+      (<Row gutter={[4, 8]} justify="space-around">
+        <Col key="nextStep">
+          <Tooltip placement="bottom" title="Move to next step" arrowPointAtCenter>
+            <Button
+              id="nextStep"
+              type="primary"
+              shape="round"
+              size="large"
+              icon={icons["next"]}
+              onClick={this.props.handleOk}
+            >
+              Next
+            </Button>
+          </Tooltip>
+        </Col>
+      </Row>)
 
 		const sliders1 = [elements["brightness"], elements["contrast"]];
 		const sliders2 = [elements["saturation"], elements["hue"]];
-		const imgCommands = [
-			elements["undo"],
-			elements["redo"],
-			elements["addMinutia"],
-			elements["resetImage"],
-		];
-		const firstRow = [elements["fpsUp"]];
-		const secondRow = [ elements["fpsSet"]];
-		const thirdRow = [
-			elements["fpsDown"],
-		];
-		const lastRow = [elements["submitImage"]];
+		const imgCommands = [elements["undo"], elements["redo"], elements["addMinutia"], elements["resetImage"]];
+		const firstRow = [elements["leftUp"], elements["up"], elements["rightUp"]];
+		const secondRow = [elements["left"], elements["fire"], elements["right"]];
+		const thirdRow = [ elements["leftDown"], elements["down"], elements["rightDown"]];
+    const fpsRow = [elements["fpsUp"], elements["fpsSet"], elements["fpsDown"]]
+		const lastRow = [elements["submitImage"], elements["start"], elements["stop"], elements["reset"]];
 
-		UIFiltered.forEach((ele, idx) => {
-			if (idx % 3 === 0) {
-				firstRow.push(elements[ele]);
-			} else if (idx % 3 === 1) {
-				secondRow.push(elements[ele]);
-			} else if (idx % 3 === 2) {
-				thirdRow.push(elements[ele]);
-			}
-		});
+		// UIFiltered.forEach((ele, idx) => {
+		// 	if (idx % 3 === 0) {
+		// 		firstRow.push(elements[ele]);
+		// 	} else if (idx % 3 === 1) {
+		// 		secondRow.push(elements[ele]);
+		// 	} else if (idx % 3 === 2) {
+		// 		thirdRow.push(elements[ele]);
+		// 	}
+		// });
 
 		return (
 			<div data-testid="control-panel">
@@ -255,7 +236,7 @@ class ControlPanel extends React.Component {
 						<div className="panelContainer">
 							{instructions.length ?
                 <div>
-                  <Divider>Instructions </Divider>
+                  <Divider>Instructions</Divider>
                   <Row gutter={[4, 8]} justify="start" className="instructions">
                     <ul>{instructionUI}</ul>
                   </Row>
@@ -273,32 +254,14 @@ class ControlPanel extends React.Component {
                   <Row gutter={[4, 8]} justify="space-between">
                     {sliders2}
                   </Row>
-                  <Row gutter={[4, 8]} justify="space-around">
-                    {lastRow}
-                  </Row>
                 </>
               :null}
+              <Row gutter={[4, 8]} justify="space-around">{fpsRow}</Row>
 							<Row gutter={[4, 8]} className="row">{firstRow}</Row>
 							<Row gutter={[4, 8]} className="row">{secondRow}</Row>
 							<Row gutter={[4, 8]} className="row">{thirdRow}</Row>
-                {isEnd ? (
-                  <Row gutter={[4, 8]} className="row">
-                  <Col key="nextStep">
-                  <Tooltip placement="bottom" title="Move to next step" arrowPointAtCenter>
-                    <Button
-                      id="nextStep"
-                      type="primary"
-                      shape="round"
-                      size="large"
-                      icon={icons["next"]}
-                      onClick={this.props.handleOk}
-                    >
-                      Next
-                    </Button>
-                  </Tooltip>
-                  </Col>
-                  </Row>
-                ) : null}
+              <Row gutter={[4, 8]} justify="space-around">{lastRow}</Row>
+              {isEnd ? next : null}
 						</div>
 					</div>
 				)}
