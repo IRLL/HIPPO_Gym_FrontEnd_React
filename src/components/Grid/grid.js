4058c5fab3113f2b9e5dd1bdfca6b55f2c0e69f7
Nadeen <nadeen@ualberta.ca>
2021-07-06 23:41:37 -0600

    cleaned up grid class and added ability to toggle select

--- src/components/Grid/grid.js
+++ src/components/Grid/grid.js
@@ -2,89 +2,91 @@ import React from "react";
 import "./grid.css";
 
 class Grid extends React.Component {
-  render() {
+  state = {
+    tiles: [],
+    tile_size: 50,
+    background_color: "white",
+    rows: 0,
+    columns: 0,
+  };
+
+  componentDidMount() {
     const { grid } = this.props;
-    const { rows, columns, tiles } = grid;
-    const tile_size = 50;
+    const { rows, columns, tiles: specialTiles } = grid;
+    const tiles = [];
+
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns; j++) {
+        tiles.push({ row: i, col: j });
+      }
+    }
+
+    for (let tile of specialTiles) {
+      const index = tile.row * columns + tile.col;
+
+      tiles[index] = tile;
+    }
+
+    this.setState({ tiles, rows, columns });
+  }
+
+  handleClick = (i) => {
+    const tiles = [...this.state.tiles];
+
+    tiles[i] = { ...tiles[i], selected: !tiles[i].selected };
+
+    this.setState({ tiles });
+  };
+
+  render() {
+    const { tiles, rows, columns, tile_size, background_color } = this.state;
 
     return (
       <div
         className="grid"
         style={{
-          position: "relative",
           width: columns * tile_size,
           height: rows * tile_size,
+          backgroundColor: background_color,
         }}
       >
-        {tiles.map((tile, i) => {
-          if (tile.type === "text")
-            return (
-              <TextTile
-                size={tile_size}
-                text={tile.value}
-                x={tile.col * tile_size}
-                y={tile.row * tile_size}
-                key={i}
-              />
-            );
-          if (tile.type === "color")
-            return (
-              <ColorTile
-                size={tile_size}
-                color={tile.value}
-                x={tile.col * tile_size}
-                y={tile.row * tile_size}
-                key={i}
-              />
-            );
-        })}
+        {tiles.map((tile, i) => (
+          <Tile
+            size={tile_size}
+            x={tile.col * tile_size}
+            y={tile.row * tile_size}
+            text={tile.text}
+            color={tile.color}
+            selected={tile.selected}
+            key={i}
+            index={i}
+            handleClick={this.handleClick}
+          />
+        ))}
       </div>
     );
   }
 }
 
-class TextTile extends React.Component {
+class Tile extends React.Component {
   render() {
-    const { size, text, x, y } = this.props;
+    const { size, color, text, x, y, selected, index, handleClick } =
+      this.props;
+
     return (
       <div
-        width={size}
-        height={size}
+        className={`tile noselect ${selected ? "selectedTile" : ""}`}
         style={{
           width: size,
           height: size,
-          position: "absolute",
+          backgroundColor: color || "transparent",
           top: y,
           left: x,
-          outline: "solid 1px black",
-          display: "grid",
-          placeItems: "center",
         }}
+        onClick={() => handleClick(index)}
       >
         {text}
       </div>
-    );
-  }
-}
-
-class ColorTile extends React.Component {
-  render() {
-    const { size, color, x, y } = this.props;
-
-    return (
-      <div
-        width={size}
-        height={size}
-        style={{
-          width: size,
-          height: size,
-          backgroundColor: color,
-          position: "absolute",
-          top: y,
-          left: x,
-          outline: "solid 1px black",
-        }}
-      />
     );
   }
 }
