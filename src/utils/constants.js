31cde2b7f6df4b33f4ecb2d7eef8c762c7c2cfce
cmuslima <musliman@ualberta.ca>
2022-11-13 12:42:29 -0700

    debugging web socket

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -22,8 +22,8 @@ export const REDIRECT = SERVER && redirect ? redirect : default_redirect;
 export const PROJECT_ID = SERVER ? "" : params.get("projectId");
 export const CSS_PATH = params.get("css");
 export const USER_ID = SERVER && userId ? userId : uuidv4();
-// export const WS_URL = SERVER ? SERVER : `wss://${USER_ID}.${host}:5005`;
-export const WS_URL = SERVER ? SERVER : `ws://localhost:5005`;
+export const WS_URL = SERVER ? SERVER : `wss://${USER_ID}.${host}:5000`;
+//export const WS_URL = SERVER ? SERVER : `ws://localhost:5005`;
 export const DEBUG = debug === "true" ? true : false;
 
 //api endpoint used to send GET and POST requests
