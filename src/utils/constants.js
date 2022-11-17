b0a50e3d82f77e4266e5a223945022ff7b6b54b3
cmuslima <musliman@ualberta.ca>
2022-11-16 19:04:35 -0700

    fixed to work locally

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,7 +10,7 @@ let redirect = params.get("redirect");
 let debug = params.get("debug");
 console.log("HOST before", host)
 if (host === "localhost" || host === "127.0.0.1") {
-	host = "testing.irll.net";
+	host = "irll.net";
 } else if (host.includes("testing")) {
 	host = host.replace("testing.", "");
 } else if (host.includes("fingerprint")) {
@@ -23,8 +23,8 @@ export const REDIRECT = SERVER && redirect ? redirect : default_redirect;
 export const PROJECT_ID = SERVER ? "" : params.get("projectId");
 export const CSS_PATH = params.get("css");
 export const USER_ID = SERVER && userId ? userId : uuidv4();
-export const WS_URL = SERVER ? SERVER : `wss://${USER_ID}.${host}:5000`;
-// export const WS_URL = SERVER ? SERVER : `ws://localhost:5005`;
+// export const WS_URL = SERVER ? SERVER : `wss://${USER_ID}.${host}:5000`;
+export const WS_URL = SERVER ? SERVER : `ws://localhost:5005`;
 export const DEBUG = debug === "true" ? true : false;
 
 //api endpoint used to send GET and POST requests
